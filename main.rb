ccf1180ecccb3a10449c03840bffff6b3598f570
Aaron George <aaronwgeorge@gmail.com>
2015-03-20 22:30:13 -0700

    Chrome fix

--- main.rb
+++ main.rb
@@ -2,7 +2,9 @@ require 'rubygems'
 require 'sinatra'
 require 'pry'
 
-set :sessions, true
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :path => '/',
+                           :secret => 'asdflkj'
 
 helpers do
   def calc_total(cards)
